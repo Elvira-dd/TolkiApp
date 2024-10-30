efe0a98f0eff06af5fa75d0a0948d6c7fefd2658
Elvira-dd <111658129+Elvira-dd@users.noreply.github.com>
2024-10-30 05:20:49 +0300

    Initial Commit

---
+++ TolkiAppTests/TolkiAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  TolkiAppTests.swift
+//  TolkiAppTests
+//
+//  Created by Эльвира on 30.10.2024.
+//
+
+import Testing
+@testable import TolkiApp
+
+struct TolkiAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
